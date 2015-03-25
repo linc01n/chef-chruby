36e4fa059dedc56ed556e792415b42ffa482dffb
Lincoln Lee <git@lincoln.hk>
2015-03-25 15:54:02 +0800

    Use 2.2.0 as default rubies

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-default['chruby']['version'] = '0.3.4'
+default['chruby']['version'] = '0.3.9'
 default['chruby']['gpg_check'] = false
 default['chruby']['use_rvm_rubies'] = false
 default['chruby']['use_rbenv_rubies'] = false
 default['chruby']['auto_switch'] = true
-default['chruby']['rubies'] = {'1.9.3-p392' => true}
+default['chruby']['rubies'] = {'2.2.0' => true}
 default['chruby']['default'] = 'embedded'
 default['chruby']['user_rubies'] = []
 default['chruby']['sh_dir'] = "/etc/profile.d"
